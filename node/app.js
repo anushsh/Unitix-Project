fa6111d5dcb8e354dc22bad1ce75f228e9238291
Michael Hackett <hackett1@sas.upenn.edu>
2020-03-08 21:41:29 -0400

    Starter code for node

---
+++ node/app.js
@@ -0,0 +1,26 @@
+// imports
+var express = require('express');
+var session = require('express-session');
+var routes = require('./routes/routes.js');
+
+
+var app = express();
+var http = require('http').createServer(app);
+var port = 3000;
+
+app.use(express.bodyParser());
+app.use(express.logger("default"));
+
+
+app.use(session({
+	resave: false,
+	saveUnitialized: false,
+	secret: "don't tell!"
+}));
+
+// route definitions
+app.get('/', routes.get_splash);
+
+// run server - we use the http server we made so that express doesn't make a new one and ignore socket.io
+http.listen(port);
+console.log(`Unitix server running on ${port}!`);
---
+++ node/routes/routes.js
@@ -0,0 +1,10 @@
+var getSplash = function (req, res) {
+
+    // TODO: Implement for real
+    res.render('splash.ejs')
+}
+
+
+module.exports = {
+    get_splash: getSplash
+}
