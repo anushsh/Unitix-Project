cfd36aa0f1bff69eb3601fc34d9f51fedc6fdfb6
Noah <noahsylv@gmail.com>
2020-04-01 16:51:51 -0400

    can now see list of events in dashboard (SUPER TERRIBLE UI)

--- android/app/src/main/java/com/example/unitix/DashboardActivity.java
+++ android/app/src/main/java/com/example/unitix/DashboardActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 
 public class DashboardActivity extends AppCompatActivity {
@@ -16,7 +18,18 @@ public class DashboardActivity extends AppCompatActivity {
         setContentView(R.layout.activity_dashboard);
         Log.e("NOAH", "here?");
         ds = new DataSource();
-        ds.getAllEvents();
+        Event[] events = ds.getAllEvents();
+        addEventsToPage(events);
+        Log.e("NOAH", "IN DASHBOARD, got " + events.length + " events");
+    }
+
+    void addEventsToPage(Event[] events) {
+        LinearLayout feed = findViewById(R.id.event_feed);
+        for (int i = 0; i < events.length; i++) {
+            TextView text = new TextView(getApplicationContext());
+            text.setText(events[i].toString());
+            feed.addView(text);
+        }
     }
 
 
