1b8f81caf2d3e4a3b3126a06538a6c9ce3afd599
Noah <noahsylv@gmail.com>
2020-04-05 19:33:08 -0400

    cleaned up current events view

--- node/views/static/helper.js
+++ node/views/static/helper.js
@@ -15,3 +15,33 @@
     var leftSide = "<button " + (bulmaClass ? 'class ="' + bulmaClass + '"' : '');
     return leftSide + clickPart+">"+buttonText+"</button>";
  }
+
+ var prettyTime = function(time) {
+    var parts = time.split(":");
+    var hour = parseInt(parts[0], 10);
+    var min = parts[1];
+    var end = "AM";
+    // fix 24 hour clock
+    if (hour > 12) {
+        end = "PM";
+        hour = hour % 12;
+    }
+    // fix integer minute parsing
+    if (min.length < 2) {
+        min = "0" + min;
+    }
+    return hour + ":" + min + " " + end;
+
+
+
+    return time;
+ }
+
+ var prettyDate = function(date) {
+    const monthNames = [ "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December" ];
+    var dateTrim = date.split("T")[0];
+    var parts = dateTrim.split("-");
+    parts[1] = monthNames[parseInt(parts[1], 10)]
+    return [parts[1], parts[2], parts[0]].join(" ");
+ }
