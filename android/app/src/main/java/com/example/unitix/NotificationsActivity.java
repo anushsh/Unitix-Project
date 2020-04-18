7e65ec2dc85e8fbf58e93fbe7283ae6b53335b42
Yash Killa <yashkilla@Yashs-MacBook-Pro-3.local>
2020-04-19 04:50:19 +0530

    More stuff for notificaitons on android

--- android/app/src/main/java/com/example/unitix/NotificationsActivity.java
+++ android/app/src/main/java/com/example/unitix/NotificationsActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -35,6 +36,11 @@ public class NotificationsActivity extends AppCompatActivity  {
         AsyncTask<Integer,Integer, Notification[]> task = new HandleNotificationsTask();
         // allow for parallel execution
         task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+        Log.d("YASH","Unread are shown");
+
+        displayReadNotifications();
+        Log.d("YASH","Here 10 - Displayed read");
+
     }
 
     private class HandleNotificationsTask extends AsyncTask<Integer, Integer, Notification[]> {
@@ -47,6 +53,21 @@ public class NotificationsActivity extends AppCompatActivity  {
         }
     }
 
+    private class HandleReadNotificationsTask extends AsyncTask<Integer, Integer, Notification[]> {
+        protected Notification[] doInBackground(Integer... ints) {
+            Log.d("YASH","Inside handlereadnotifications doinbackground");
+            Notification[] allReadNotifications = ds.getAllReadNotifications(user.getId());
+            Log.d("YASH","read notif length = " + allReadNotifications.length);
+            return allReadNotifications;
+        }
+        protected void onPostExecute(Notification[] notifications) {
+            Log.d("YASH","on postexecute for handlereadnotif");
+
+            Log.d("YASH","read notification view received notifications, got" + notifications.length);
+            addReadNotificationsToPage(notifications);
+        }
+    }
+
     void addNotificationsToPage(Notification[] notifications) {
         LinearLayout feed = findViewById(R.id.notification_list);
         for (Notification notification : notifications) {
@@ -55,12 +76,25 @@ public class NotificationsActivity extends AppCompatActivity  {
             notificationText.setTextSize(30);
             feed.addView(notificationText);
         }
-        if (notifications.length == 0) {
-            hideReadNotificationsButton();
+//        if (notifications.length == 0) {
+//            Log.d("YASH","Come here only if no unread");
+//            hideReadNotificationsButton();
+//        }
+    }
+
+    void addReadNotificationsToPage(Notification[] notifications) {
+        Log.d("YASH","Here 5 - adding read notifications");
+        LinearLayout feed = findViewById(R.id.read_notification_list);
+        for (Notification notification : notifications) {
+            TextView notificationText = new TextView(getApplicationContext());
+            notificationText.setText(notification.content);
+            notificationText.setTextSize(30);
+            feed.addView(notificationText);
         }
     }
 
     void hideReadNotificationsButton() {
+        Log.d("YASH","Come here only if no unread 2");
         Button button = findViewById(R.id.readnotificationsbtn);
         button.setVisibility(View.INVISIBLE);
         LinearLayout feed = findViewById(R.id.notification_list);
@@ -69,13 +103,17 @@ public class NotificationsActivity extends AppCompatActivity  {
         notificationText.setTextSize(18);
         feed.addView(notificationText);
     }
+
     void updateNotificationsButton() {
+        Log.d("YASH","Come here only if read button clicked");
         Button button = findViewById(R.id.readnotificationsbtn);
         button.setVisibility(View.INVISIBLE);
         TextView header1 = findViewById(R.id.headerUnread);
         TextView header2 = findViewById(R.id.headerRead);
         header1.setVisibility(View.INVISIBLE);
         header2.setVisibility(View.INVISIBLE);
+        LinearLayout readNotifications = findViewById(R.id.read_notification_list);
+        readNotifications.setVisibility(View.INVISIBLE);
 
         LinearLayout feed = findViewById(R.id.notification_list);
         TextView notificationText = new TextView(getApplicationContext());
@@ -90,7 +128,14 @@ public class NotificationsActivity extends AppCompatActivity  {
         feed.removeAllViews();
         updateNotificationsButton();
     }
+    
+    public void displayReadNotifications() {
+        Log.d("YASH","Here 7 - in display function");
 
+        AsyncTask<Integer,Integer, Notification[]> taskRead = new HandleReadNotificationsTask();
+        taskRead.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+        Log.d("YASH","Here 9 - after executing readnotification async task");
 
+    }
 
 }
