688516ab59790d63977d106396eb9a0a1982d30c
Kara Li <likara@seas.upenn.edu>
2020-04-19 01:12:02 -0400

    added minor changes to Group Page Activity (android)

--- android/app/src/main/java/com/example/unitix/GroupPageActivity.java
+++ android/app/src/main/java/com/example/unitix/GroupPageActivity.java
@@ -17,6 +17,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.unitix.models.Event;
 import com.example.unitix.models.Show;
+import com.example.unitix.models.User;
 import com.example.unitix.server.DataSource;
 import com.example.unitix.R;
 import com.example.unitix.models.Group;
@@ -26,14 +27,13 @@ public class GroupPageActivity extends AppCompatActivity {
     DataSource ds;
     Event event;
     Group group;
+    User user;
+    private String email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group);
-
-        this.ds = new DataSource();
-
         Intent intent = getIntent();
         String name = intent.getStringExtra("groupName");
         String bio = intent.getStringExtra("bio");
@@ -43,6 +43,13 @@ public class GroupPageActivity extends AppCompatActivity {
         groupName.setText(name);
         groupBio.setText(bio);
 
+        this.ds = new DataSource();
+        email = intent.getStringExtra("EMAIL");
+        this.user = this.ds.getUser(email);
+    }
+
+    void checkFollowingStatus() {
+       // user.followers
     }
 
 }
